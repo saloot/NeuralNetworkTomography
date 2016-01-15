c9b9ffe2d19286f46b1fa08b4ed56c8e989b1e12
Amir Hesam Salavati <saloot@gmail.com>
2016-01-15 11:02:33 +0100

    small bug fixed

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -2408,7 +2408,7 @@ def delayed_inference_constraints_numpy(out_spikes_tot_mat_file,TT,n,max_itr_opt
             lam = np.reshape(res_cons['x'],[TcT,1])
             ww = np.dot(AA.T,lam)
             ww2 = np.dot(C_i,Z + 0.5*ww[0:n])
-            ww2 = ww2/np.linalg.norm(ww2)
+            ww2 = ww2/(0.001+np.linalg.norm(ww2))
             Z = soft_threshold(ww2,sparse_thr_0)
         #----------------------------------------------------------
         
@@ -2502,7 +2502,7 @@ def delayed_inference_constraints_numpy(out_spikes_tot_mat_file,TT,n,max_itr_opt
                         lam = np.reshape(res_cons['x'],[TcT,1])
                         ww = np.dot(AA.T,lam)
                         ww2 = np.dot(C_i,Z + 0.5*ww[0:n])
-                        ww2 = ww2/np.linalg.norm(ww2)
+                        ww2 = ww2/(0.0001+np.linalg.norm(ww2))
                         Z = soft_threshold(ww2,sparse_thr)
                         #if sum(Z) == 0:
                         #    pdb.set_trace()
