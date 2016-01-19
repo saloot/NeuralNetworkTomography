a1f093a17d7c30a348f57c9043011f493d793900
Amir Hesam Salavati <saloot@gmail.com>
2016-01-19 15:09:09 +0100

    Some changes made to make the probem more appealing

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -2528,7 +2528,7 @@ def delayed_inference_constraints_numpy(out_spikes_tot_mat_file,TT,n,max_itr_opt
                         lam = np.reshape(res_cons['x'],[TcT,1])
                         ww = np.dot(AA.T,lam)
                         ww2 = np.dot(C_i,Z + 0.5*ww[0:n])
-                        pdb.set_trace()
+                        #pdb.set_trace()
                         ww2 = ww2/(0.0001+np.linalg.norm(ww2))
                         Z = soft_threshold(ww2,sparse_thr)
                         #if sum(Z) == 0:
