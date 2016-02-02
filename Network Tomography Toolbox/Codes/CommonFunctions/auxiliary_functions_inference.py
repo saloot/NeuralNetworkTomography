2471e22438c505920021632eee236910c9c594a5
Amir Hesam Salavati <saloot@gmail.com>
2016-02-02 11:28:46 +0100

    small bug changes

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -2927,9 +2927,9 @@ def delayed_inference_constraints_svm(out_spikes_tot_mat_file,TT,n,max_itr_opt,s
                 u = v-x
                 #..................................................................
             
-            if itr_W:
+            if itr_W > 1:
                 W2 = W2 + np.reshape(W_infer[0:itr_W,:].mean(axis = 0),[n+1,1])
-            else:
+            elif itr_W == 1:
                 W2 = W2 + np.reshape(W_infer[0:itr_W,:],[n+1,1])
             pdb.set_trace()
             #Z = (Z>4*sparse_thr).astype(int) - (Z<-4*sparse_thr).astype(int)   
