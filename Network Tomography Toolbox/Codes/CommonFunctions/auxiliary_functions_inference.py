5e069d25641083def0bfada68172abcd1b17520c
Amir Hesam Salavati <saloot@gmail.com>
2016-02-17 15:38:11 +0100

    Duality gap added

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -2449,8 +2449,9 @@ def delayed_inference_constraints_numpy(out_spikes_tot_mat_file,TT,n,max_itr_opt
         for i in range(0,2):
             #BB = eta * (np.dot(AA,Z) + eta * np.dot(np.dot(AA,Cc),Z))
             #BB = np.dot(AA,np.dot(C_i,Z))
-            BB = delta * np.dot(np.eye(TcT) + theta * np.dot(np.diag(g.ravel())),np.ones([TcT,1]))
             pdb.set_trace()
+            BB = delta * np.dot(np.eye(TcT) + theta * np.dot(np.diag(g.ravel())),np.ones([TcT,1]))
+            
             res_cons = optimize.minimize(loss_func_lambda, lambda_0, args=(FF,BB),jac=jac_lambda,bounds=bns,constraints=(),method='L-BFGS-B', options=opt)
             # res_cons['status']
             # res_cons['message']
