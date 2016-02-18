66f21ba565bbf320d7bb17676a4027f20cc9018a
Amir Hesam Salavati <saloot@gmail.com>
2016-02-18 13:41:33 +0100

    Duality gap added

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -2825,9 +2825,9 @@ def delayed_inference_constraints_numpy(out_spikes_tot_mat_file,TT,n,max_itr_opt
             WW = np.zeros([len_v,1])
             
             st_cof = 0.1/float(1+ttau)
-            W_tot = W_tot + st_cof * Delta_W/no_blocks
+            W_tot = W_tot + 0.02 * Delta_W/no_blocks
             #W_tot = W_tot - W_tot.mean()
-            W_tot = W_tot/np.linalg.norm(W_tot)
+            #W_tot = W_tot/np.linalg.norm(W_tot)
             #W_tot = W_tot/np.linalg.norm(W_tot)
             WW[0:ijk,0] = W_tot[0:ijk,0]
             WW[ijk+1:,0] = W_tot[ijk:,0]
