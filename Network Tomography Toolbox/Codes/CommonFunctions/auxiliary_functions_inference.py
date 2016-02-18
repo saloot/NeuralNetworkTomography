554a8d9f0b2be14e10b8ffd3fa4b1bcf441f1d1b
Amir Hesam Salavati <saloot@gmail.com>
2016-02-18 19:53:09 +0100

    a new approach based on hinge loss is being tested

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -1494,7 +1494,7 @@ def hinge_jac(x,FF,b,avg,lamb):
     #temp = ((np.dot(FF,x) + b)>0).astype(int)
     print temp.max(),temp.shape
     tmp = avg*np.dot(FF.T,temp).ravel()
-    print np.linalg.norm(tmp)
+    print np.linalg.norm(tmp),np.linalg.norm(2*lamb*x)
     tmp = tmp.ravel() + 2*lamb*x.ravel()
     print np.linalg.norm(tmp)
     return tmp
