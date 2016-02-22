25e96cdf36cc95d38ec442090f51b43b49ae1657
Amir Hesam Salavati <saloot@gmail.com>
2016-02-22 17:05:55 +0100

    The code is edited to make the hinge based loss much faster

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -3420,7 +3420,7 @@ def delayed_inference_constraints_hinge(out_spikes_tot_mat_file,TT,n,max_itr_opt
                 
                 #-----------------------Do the Optimization---------------------
                 TcT = len(yy)
-                lamb = .005/float(TcT)
+                lamb = .01/float(TcT)
                 cf = lamb*TcT
                         
                 lambda_temp = lambda_tot[block_count*block_size:(block_count+1)*block_size]
@@ -3432,7 +3432,7 @@ def delayed_inference_constraints_hinge(out_spikes_tot_mat_file,TT,n,max_itr_opt
                 d_alp_vec = np.zeros([block_size,1])
                 
                         
-                for ss in range(0,10*TcT):
+                for ss in range(0,100*TcT):
                             
                     ii = np.random.randint(0,TcT)
                     jj = t_inds[ii]
