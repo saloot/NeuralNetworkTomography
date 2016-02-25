a small bug is fixed for the inference algorithm precision

Network Tomography Toolbox/Codes/Inference_Tomography.py
@@ -261,7 +261,10 @@ for T in T_range:
     else:        
         for ik in neuron_range:
             file_name =  file_name_base_results + "/Inferred_Graphs/W_Pll_%s_%s_%s.txt" %(file_name_prefix,file_name_ending,str(ik))
-            np.savetxt(file_name,WW[:,ik].T,'%2.5f',delimiter='\t')
+            tmp = WW[:,ik]
+            tmp = tmp/np.linal.norm(tmp)
+            tmp = tmp/(np.abs(tmp).max())
+            np.savetxt(file_name,tmp.T,'%2.6f',delimiter='\t')
 
     #..........................................................................
     