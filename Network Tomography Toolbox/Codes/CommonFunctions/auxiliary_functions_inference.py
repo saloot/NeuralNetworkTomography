fbd41fd8169bb965b8a8fb9f2ae5d09803d4c17c
Amir Hesam Salavati <saloot@gmail.com>
2016-03-01 20:43:13 +0100

    experimenting l1 loss

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -3253,7 +3253,7 @@ def delayed_inference_constraints_hinge(out_spikes_tot_mat_file,TT,n,max_itr_opt
     if TT > 20000:
         
         T_temp = 50                              # The size of the initial batch to calculate the initial inverse matrix
-        block_size = 30000
+        block_size = 25000
         T0 = max(TT - 3*block_size,50)                                  # It is the offset, i.e. the time from which on we will consider the firing activity
     else:
         T0 = 0
@@ -3461,7 +3461,7 @@ def delayed_inference_constraints_hinge(out_spikes_tot_mat_file,TT,n,max_itr_opt
                 
                 #-----------------------Do the Optimization---------------------
                 TcT = len(yy)
-                lamb = .1/float(TcT)
+                lamb = .001/float(TcT)
                 cf = lamb*TcT
                         
                 lambda_temp = lambda_tot[block_count*block_size:(block_count+1)*block_size]
@@ -3483,11 +3483,14 @@ def delayed_inference_constraints_hinge(out_spikes_tot_mat_file,TT,n,max_itr_opt
                 bb = np.dot(bb,aa)
                 #pdb.set_trace()
                 #bb = aa
-                #FF = np.dot(bb,bb.T)
-                FF = bb.T
+                
+                
                 cb = np.ones([TcT,1]) - 1 * np.dot(bb,W_tot) 
                 opt = {'disp':False,'maxiter':5000}
-                res_cons = optimize.minimize(hinge_loss_func_dual, lambda_0, args=(FF,cb,0.5/cf),jac=hinge_jac_dual,bounds=bns,constraints=(),method='L-BFGS-B', options=opt)
+                #FF = bb.T
+                #res_cons = optimize.minimize(hinge_loss_func_dual, lambda_0, args=(FF,cb,0.5/cf),jac=hinge_jac_dual,bounds=bns,constraints=(),method='L-BFGS-B', options=opt)
+                FF = np.dot(bb,bb.T)
+                res_cons = optimize.minimize(hinge_loss_func_dual_l2, lambda_0, args=(FF,cb,0.5/cf),jac=hinge_jac_dual_l2,bounds=bns,constraints=(),method='L-BFGS-B', options=opt)
                 print res_cons['message']
                 d_alp_vec[t_inds] = np.reshape(res_cons['x'],[TcT,1])
                 
@@ -3548,7 +3551,7 @@ def delayed_inference_constraints_hinge(out_spikes_tot_mat_file,TT,n,max_itr_opt
                 Yk = (YY>0).astype(int)
                 cst = np.dot(DD,2*W_tot)
                 
-                pdb.set_trace()
+                #pdb.set_trace()
                 #cst = np.dot(DD,2*Delta_W)
                 total_cost[ttau] = total_cost[ttau] + sum(np.sign(cst.ravel())!=np.sign(YY))
                 
