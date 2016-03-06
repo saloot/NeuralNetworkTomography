e9c8dd715d45b164007c3e215c3cbabd7a963780
Amir Hesam Salavati <saloot@gmail.com>
2016-03-06 02:00:56 +0100

    trying to debug

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -3351,7 +3351,7 @@ def delayed_inference_constraints_hinge(out_spikes_tot_mat_file,TT,n,max_itr_opt
                 #-------------------------------------------------
                 
                 #--------------Check If the Block Is Processed Before-----------
-                spikes_file = out_spikes_tot_mat_file[:-4] + '_b_' + str(block_size) + '_c_' + str(block_count) + '_i_' + str(ijk) + '_A.txt'
+                spikes_file = out_spikes_tot_mat_file[:-4] + '_b_' + str(block_size) + '_c_' + str(t_0) + '_i_' + str(ijk) + '_A.txt'
                 if not os.path.isfile(spikes_file):
                     X = np.zeros([len_v,block_size])
                     V = np.zeros([len_v,block_size])
@@ -3425,17 +3425,17 @@ def delayed_inference_constraints_hinge(out_spikes_tot_mat_file,TT,n,max_itr_opt
                         print 'Integrate file could not be saved'
                         
                     try:
-                        spikes_file = out_spikes_tot_mat_file[:-4] + '_b_' + str(block_size) + '_c_' + str(block_count) + '_i_' + str(ijk) + '_Y.txt'
+                        spikes_file = out_spikes_tot_mat_file[:-4] + '_b_' + str(block_size) + '_c_' + str(t_0) + '_i_' + str(ijk) + '_Y.txt'
                         np.savetxt(spikes_file,YY,'%2.1f',delimiter='\t')
                     except:
                         print 'Spikes file could not be saved'
                     #----------------------------------------------------------
                 else:
                     print 'yoohoo!'
-                    spikes_file = out_spikes_tot_mat_file[:-4] + '_b_' + str(block_size) + '_c_' + str(block_count) + '_i_' + str(ijk) + '_A.txt'
+                    spikes_file = out_spikes_tot_mat_file[:-4] + '_b_' + str(block_size) + '_c_' + str(t_0) + '_i_' + str(ijk) + '_A.txt'
                     AA = np.genfromtxt(spikes_file, dtype=float, delimiter='\t')
                     
-                    spikes_file = out_spikes_tot_mat_file[:-4] + '_b_' + str(block_size) + '_c_' + str(block_count) + '_i_' + str(ijk) + '_Y.txt'
+                    spikes_file = out_spikes_tot_mat_file[:-4] + '_b_' + str(block_size) + '_c_' + str(t_0) + '_i_' + str(ijk) + '_Y.txt'
                     YY = np.genfromtxt(spikes_file, dtype=float, delimiter='\t')
                 #---------------------------------------------------------------
                 
