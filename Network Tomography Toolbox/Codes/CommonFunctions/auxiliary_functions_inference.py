6845c95f711c2adfdd8aa4d27d151211f85b9b3f
Amir Hesam Salavati <saloot@gmail.com>
2016-03-06 15:39:01 +0100

    trying to debug

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -3509,7 +3509,7 @@ def delayed_inference_constraints_hinge(out_spikes_tot_mat_file,TT,n,max_itr_opt
                 #lam = np.reshape(res_cons['x'],[TcT,1])
                 #lam = np.multiply(lam,(lam > qq.min()).astype(int))
                 #d_alp_vec[t_inds] = lam
-                for ss in range(0,10*TcT):
+                for ss in range(0,100*TcT):
                             
                     ii = np.random.randint(0,TcT)
                     jj = t_inds[ii]
