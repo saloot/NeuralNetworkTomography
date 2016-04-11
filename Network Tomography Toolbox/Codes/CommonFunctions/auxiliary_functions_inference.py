3b03da501da0923a922f2f0c707c36e2520f5935
Amir Hesam Salavati <saloot@gmail.com>
2016-04-11 10:08:17 +0200

    Experimenting with different parameters

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -3480,7 +3480,7 @@ def delayed_inference_constraints_hinge(out_spikes_tot_mat_file,TT,n,max_itr_opt
                 
                 #-----------------------Do the Optimization---------------------
                 TcT = len(yy)
-                lamb = .001/float(TcT)
+                lamb = .0001/float(TcT)
                 cf = lamb*TcT
                 
                 #pdb.set_trace()        
@@ -3517,7 +3517,7 @@ def delayed_inference_constraints_hinge(out_spikes_tot_mat_file,TT,n,max_itr_opt
                 #lam = np.reshape(res_cons['x'],[TcT,1])
                 #lam = np.multiply(lam,(lam > qq.min()).astype(int))
                 #d_alp_vec[t_inds] = lam
-                for ss in range(0,250*TcT):
+                for ss in range(0,500*TcT):
                             
                     ii = np.random.randint(0,TcT)
                     jj = t_inds[ii]
