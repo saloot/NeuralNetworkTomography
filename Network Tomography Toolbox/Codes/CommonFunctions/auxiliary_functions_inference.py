cd1fe3af629a219c04674c58b508c0de2c2f274f
Amir Hesam Salavati <saloot@gmail.com>
2016-04-13 11:09:14 +0200

    minor change

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -1022,6 +1022,7 @@ def read_spikes_lines_integrated(file_name,line_no,n):
         return a
             
     else:
+        pdb.set_trace()
         return []
     
     
@@ -3554,7 +3555,7 @@ def delayed_inference_constraints_hinge(out_spikes_tot_mat_file,TT,n,max_itr_opt
                     aa_t = read_spikes_lines_integrated(spikes_file_AA,ii,n)
                     yy_t = read_spikes_lines_integrated(spikes_file_YY,ii,1)
                     
-                    ff = gg[yy_t[0]]*aa_t
+                    ff = gg[yy_t[0]]*np.array(aa_t)
                     
                     if theta:
                         c = 1 + theta * yy_t
