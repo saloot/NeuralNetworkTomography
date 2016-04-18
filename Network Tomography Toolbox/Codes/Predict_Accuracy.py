ed5aaeab1242714b316ecb86644a8a139e8c3279
Amir Hesam Salavati <saloot@gmail.com>
2016-04-18 14:44:02 +0200

    Trying to experiment with ifferent parameters

--- Network Tomography Toolbox/Codes/Predict_Accuracy.py
+++ Network Tomography Toolbox/Codes/Predict_Accuracy.py
@@ -49,13 +49,22 @@ T_max = 500000
 #T_step = int(T_max/6.0)
 #T_range = range(T_step, T_max+1, T_step)
 
-T_range = [no_stimul_rounds]
-
+T_range = [75000,175000,250000,350000,450000,550000]
+T_range = [225000,325000,425000]
+T_range = [425000,725000,1025000,1325000,1625000,1925000,2225000,2525000,2825000,3125000,3425000,4025000,4325000]
+T_range = [1925000,2225000,2525000,2825000,3125000,3425000,4025000,4325000]
+T_range = [2225000,2825000,3125000,3425000,3725000,4025000,4625000,5225000]
+T_range = [425000,725000,1025000,1325000,1625000,1925000,2225000,2525000,2825000,3125000,3425000,3725000,4025000]
+T_range = [325000,625000,925000,1225000,1525000,1825000,2125000,2425000,2725000,3025000]
+T_range = range(325000,4525001,300000)
+T_range = range(625000,3625001,600000)
 #------------------------------------------------------------------------------
 
 #----------------------Initialize the Results Matrices-------------------------
 Prediction_true_pos = np.zeros([len(T_range),1])
 Prediction_true_neg = np.zeros([len(T_range),1])
+Mean_belief = np.zeros([len(T_range),7])
+Pres_Reca = np.zeros([len(T_range),7])
 #------------------------------------------------------------------------------
 
 #-------------Specify the Name of the Files Containg the Spikes----------------
@@ -101,7 +110,7 @@ DD_act = 1.5 * abs(np.sign(W_act))
 #D_act = np.genfromtxt(file_name, dtype=None, delimiter='\t')
 #DD_act = np.multiply(np.sign(abs(W_act)),D_act)
 #DD_act = (np.ceil(1000 * DD_act)).astype(int)
-
+W_infer = np.zeros([len(T_range),len(W_act[:,n_ind])])
 #==============================================================================
 
 #=======================EVALUATE THE PREDICTION ACCURACY=======================
@@ -118,18 +127,35 @@ for T in T_range:
     if bin_size:
         file_name_ending = file_name_ending + '_bS_' + str(bin_size)
    
+    if 1:
+        file_name_ending = file_name_ending + '_ii_' + str(max_itr_optimization)    
     file_name =  file_name_base_results + "/Inferred_Graphs/W_Pll_%s_%s_%s.txt" %(file_name_prefix,file_name_ending,str(n_ind))
-    W = np.genfromtxt(file_name, dtype=None, delimiter='\t')
-    #W = W/(0.0001+np.linalg.norm(W))
     
+    if get_from_hots:
+        cmd = 'scp salavati@deneb1.epfl.ch:"~/NeuralNetworkTomography/Network\ Tomography\ Toolbox/Results/Inferred_Graphs/W_Pll_%s_%s_%s.txt" ../Results/Inferred_Graphs/' %(file_name_prefix,file_name_ending,str(ik))                
+        os.system(cmd)
+        W_read = np.genfromtxt(file_name, dtype=None, delimiter='\t')
+        
+    W_infer[itr_T,:] = np.genfromtxt(file_name, dtype=None, delimiter='\t')
+    #W_infer[itr_T,:] = W_infer[itr_T,:]/np.linalg.norm(W_infer[itr_T,:])
+    #W_infer[itr_T,:] = W_infer[itr_T,:]-W_infer[itr_T,:].mean()
+    if itr_T:
+        W = sum(W_infer[0:itr_T+1,:])/float(itr_T+1)
+        #W = W_infer[itr_T,:]
+    else:
+        W = W_infer[itr_T,:]
+
+    
+    #W = W/np.linalg.norm(W)
     theta = W[-1]
     
     #W = W_act[:,n_ind]
     
     T_test = int(0.2*T)
-    T0 = T
-    T0 = 50
-    T_test = 30000
+    T_test = 10000
+    T0 = T + 10000
+    #T0 = 50
+    
     T_array = [[T0+100,T0+100+T_test],[2*T0+100,2*T0+100+T_test],[10*T0+100,10*T0+100+T_test]]
     T_array = [[T0,T0+T_test]]
     
@@ -141,15 +167,48 @@ for T in T_range:
         
     W = np.array(W2)
     
-    pdb.set_trace()
-    #W = W - W.mean()    
+    #W = W - W.mean()
+    
+    #-------------------------Evaluate Accuracy of Weights-------------------------
+    ww = W_act[:,n_ind]
+    W = W - W.mean()
+    W = W/np.linalg.norm(W)
+    Mean_belief[itr_T,0] = (T/1000.)
+    Mean_belief[itr_T,1] = sum(np.multiply(ww>0,W.ravel()))/sum(ww>0)
+    Mean_belief[itr_T,2] = sum(np.multiply(ww==0,W.ravel()))/sum(ww==0)
+    Mean_belief[itr_T,3] = sum(np.multiply(ww<0,W.ravel()))/sum(ww<0)
+    
+    Mean_belief[itr_T,4] = pow(sum(pow(np.multiply(ww>0,W.ravel())-Mean_belief[itr_T,1],2))/sum(ww>0),0.5)
+    Mean_belief[itr_T,5] = pow(sum(pow(np.multiply(ww==0,W.ravel())-Mean_belief[itr_T,2],2))/sum(ww==0),0.5)
+    Mean_belief[itr_T,6] = pow(sum(pow(np.multiply(ww<0,W.ravel())-Mean_belief[itr_T,3],2))/sum(ww<0),0.5)
+    
+    
+    W_b = (W>0.99*Mean_belief[itr_T,1]).astype(int) - (W<0.99*Mean_belief[itr_T,3]).astype(int)
+    W_b = W_b.ravel()
+    Pres_Reca[itr_T,0] = (T/1000.)
+    
+    Pres_Reca[itr_T,1] = sum(np.multiply((W_b>0).astype(int),(W_act[:,n_ind]>0).astype(int)))/float(sum((W_act[:,n_ind]>0).astype(int)))
+    Pres_Reca[itr_T,2] = sum(np.multiply((W_b==0).astype(int),(W_act[:,n_ind]==0).astype(int)))/float(sum((W_act[:,n_ind]==0).astype(int)))
+    Pres_Reca[itr_T,3] = sum(np.multiply((W_b<0).astype(int),(W_act[:,n_ind]<0).astype(int)))/float(sum((W_act[:,n_ind]<0).astype(int)))
+    
+    Pres_Reca[itr_T,4] = sum(np.multiply((W_b>0).astype(int),(W_act[:,n_ind]>0).astype(int)))/float(sum(W_b>0))
+    Pres_Reca[itr_T,5] = sum(np.multiply((W_b==0).astype(int),(W_act[:,n_ind]==0).astype(int)))/float(sum(W_b==0))
+    Pres_Reca[itr_T,6] = sum(np.multiply((W_b<0).astype(int),(W_act[:,n_ind]<0).astype(int)))/float(sum(W_b<0))
+    #------------------------------------------------------------------------------
+    
+    
     Accur_true_pos,Accur_true_neg = spike_pred_accuracy(file_name_spikes2,T_array,W,n_ind,theta)
     Prediction_true_pos[itr_T,0] = Accur_true_pos
     Prediction_true_neg[itr_T,0] = Accur_true_neg
-    itr_T = itr_T + 1    
     
-    pdb.set_trace()
+    
     print 'Evaluation is successfully completed for T = %s ms' %str(T/1000.0)
+    print 'Accuracy were: %s,%s' %(str(Prediction_true_pos[itr_T,0]),str(Prediction_true_neg[itr_T,0]))
+    print 'Mean beliefs were'
+    print Mean_belief[itr_T,1:]
+    print '-----------------------'
+    
+    itr_T = itr_T + 1    
 #------------------------------------------------------------------------------
 
 #==============================================================================
@@ -172,9 +231,21 @@ T_range = np.divide(T_range,1000).astype(int)
 temp = np.hstack([T_range,temp])
 np.savetxt(file_name,temp,'%2.5f',delimiter='\t')
 
-pdb.set_trace()
+
+file_name =  file_name_base_results + "/Plot_Results/Mean_Beliefs_%s_%s.txt" %(file_name_ending,str(n_ind))
+np.savetxt(file_name,Mean_belief,'%3.5f',delimiter='\t')
+
+
+file_name =  file_name_base_results + "/Plot_Results/Preci_Reca_%s_%s.txt" %(file_name_ending,str(n_ind))
+np.savetxt(file_name,Pres_Reca,'%3.5f',delimiter='\t')
 #==============================================================================
 
+pdb.set_trace()
+plt.plot(Mean_belief[:,0],Mean_belief[:,1],'r');plt.plot(Mean_belief[:,0],Mean_belief[:,2],'g');plt.plot(Mean_belief[:,0],Mean_belief[:,3],'b');plt.show()
+#plt.plot(Mean_belief[:,0],Mean_belief[:,4],'r');plt.plot(Mean_belief[:,0],Mean_belief[:,5],'g');plt.plot(Mean_belief[:,0],Mean_belief[:,6],'b');plt.show()
+plt.plot(Pres_Reca[:,0],Pres_Reca[:,1],'r');plt.plot(Pres_Reca[:,0],Pres_Reca[:,2],'g');plt.plot(Pres_Reca[:,0],Pres_Reca[:,3],'b');plt.show()
+plt.plot(Pres_Reca[:,0],Pres_Reca[:,4],'r');plt.plot(Pres_Reca[:,0],Pres_Reca[:,5],'g');plt.plot(Pres_Reca[:,0],Pres_Reca[:,6],'b');plt.show()
+#W_b = (W>0.07).astype(int) - (W<-0.2).astype(int)
 #--------------------------Calculate Accuracy-------------------------
 #fpr, tpr, thresholds = metrics.roc_curve(WW.ravel(),whiten(W_inferred).ravel())    
 #print('\n==> AUC = '+ str(metrics.auc(fpr,tpr))+'\n');
