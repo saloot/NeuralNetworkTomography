b0c8c43f3cf0a3967ae551ae65f41aaf60d44a7f
Amir Hesam Salavati <saloot@gmail.com>
2016-04-22 15:03:34 +0200

    minor changes

--- Network Tomography Toolbox/Codes/Inference_Tomography.py
+++ Network Tomography Toolbox/Codes/Inference_Tomography.py
@@ -81,12 +81,12 @@ file_name_spikes = '../Data/Spikes/Moritz_Spike_Times_750.txt'
 
 if (inference_method == 7):
     file_name_spikes = '../Data/Spikes/Moritz_Spike_Times.txt'
-    file_name_spikes = '/scratch/salavati/NeuralNetworkTomography/Network Tomography Toolbox/Data/Spikes/Moritz_Spike_Times.txt'
+    #file_name_spikes = '/scratch/salavati/NeuralNetworkTomography/Network Tomography Toolbox/Data/Spikes/Moritz_Spike_Times.txt'
     file_name_prefix = 'Moritz'
 elif (inference_method == 5):
     inference_method = 7
     file_name_spikes = '../Data/Spikes/HC3_ec013_198_processed.txt'
-    #file_name_spikes = '/scratch/salavati/NeuralNetworkTomography/Network Tomography Toolbox/Data/Spikes/HC3_ec013_198_processed.txt'
+    file_name_spikes = '/scratch/salavati/NeuralNetworkTomography/Network Tomography Toolbox/Data/Spikes/HC3_ec013_198_processed.txt'
     file_name_prefix = 'HC3'
     
 #file_name_spikes = '../Data/Spikes/S_times_n_80_20.txt'
