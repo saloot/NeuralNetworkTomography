4553bed2ede2bdef373481bfc0db9898b15f7c3a
Amir Hesam Salavati <saloot@gmail.com>
2016-04-25 11:50:20 +0200

    small changes

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -3711,11 +3711,12 @@ def delayed_inference_constraints_hinge(out_spikes_tot_mat_file,TT,n,max_itr_opt
                     #~~~~~~~~~~~Find the Optimal Delta-Alpha~~~~~~~~~~~
                     #aa_t = read_spikes_lines_integrated(spikes_file_AA,ii+1,n)
                     #yy_t = read_spikes_lines_integrated(spikes_file_YY,ii+1,1)
+                    #yy_t = yy_t[0]
                     aa_t = aa[ii,:]
                     yy_t = yy[ii]
                     
                     try:
-                        ff = gg[yy_t[0]]*(aa_t)
+                        ff = gg[yy_t]*(aa_t)
                     except:
                         pdb.set_trace()
                     
