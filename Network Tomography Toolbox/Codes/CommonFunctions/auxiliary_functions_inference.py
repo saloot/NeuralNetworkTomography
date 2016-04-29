883e4fa62ea7e96e4f500fa6aae1e4700571b1ff
Amir Hesam Salavati <saloot@gmail.com>
2016-04-29 14:06:29 +0200

    trying to prallelize the whole thing

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -3652,7 +3652,10 @@ def inference_constraints_hinge_parallel(out_spikes_tot_mat_file,TT,block_size,n
         for ttau in range_tau:
             
             #~~~~~~~~~~~~~~~~~~In-loop Initializations~~~~~~~~~~~~~~~~~~
-            block_start = block_start_inds[itr_block]            
+            try:
+                block_start = block_start_inds[itr_block]
+            except:
+                pdb.set_trace()
             int_results = []
             total_spent_time = 0
             tic = time.clock()
