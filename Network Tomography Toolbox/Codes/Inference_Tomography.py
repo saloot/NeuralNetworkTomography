4514587337d85891394737931850d61058c2a916
Amir Hesam Salavati <saloot@gmail.com>
2016-05-02 15:10:11 +0200

    testing various parameters for optimizaion algorithm

--- Network Tomography Toolbox/Codes/Inference_Tomography.py
+++ Network Tomography Toolbox/Codes/Inference_Tomography.py
@@ -81,7 +81,7 @@ file_name_spikes = '../Data/Spikes/Moritz_Spike_Times_750.txt'
 
 if (inference_method == 7):
     file_name_spikes = '../Data/Spikes/Moritz_Spike_Times.txt'
-    #file_name_spikes = '/scratch/salavati/NeuralNetworkTomography/Network Tomography Toolbox/Data/Spikes/Moritz_Spike_Times.txt'
+    file_name_spikes = '/scratch/salavati/NeuralNetworkTomography/Network Tomography Toolbox/Data/Spikes/Moritz_Spike_Times.txt'
     file_name_prefix = 'Moritz'
 elif (inference_method == 5):
     inference_method = 7
@@ -243,7 +243,7 @@ for T in T_range:
         import multiprocessing
 
         num_process = multiprocessing.cpu_count()
-        block_size = 200000
+        block_size = 500000
         #num_process = 4
         W_inferred = inference_constraints_hinge_parallel(file_name_spikes2,T,block_size,n,max_itr_optimization,sparse_thr0,alpha0,theta,neuron_range,num_process)
     #--------------Post-Process the Inferred Matrix---------------
