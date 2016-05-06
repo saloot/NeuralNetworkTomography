3a9abd3322fb494cea6adb0f032d8b5f64574ce3
Amir Hesam Salavati <saloot@gmail.com>
2016-05-06 12:13:41 +0200

    experimenting wih the mthd 1

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -3932,8 +3932,8 @@ def inference_constraints_hinge_parallel_CPU(out_spikes_tot_mat_file,TT,block_si
                     int_results.append(pool.apply_async(infer_w_block_integration, func_args) )
             
             
-                     calculate_integration_matrix
-                     #??
+                    #calculate_integration_matrix
+                    ##??
                     func_args = [W_tot,A,YA,gg,lambda_tot,block_count,bblock_size,rand_sample_flag,mthd,len_v]
                 #Delta_W_loc,cst,d_alp_vec,w_parallel_flag = infer_w_block(W_tot,A,YA,gg,lambda_tot,block_count,block_size,rand_sample_flag,mthd,len_v)            
                 #pdb.set_trace()
