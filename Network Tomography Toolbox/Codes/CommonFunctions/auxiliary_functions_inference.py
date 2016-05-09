7e1d59f7e424c3da5338b52a54ce9235a6da2ce2
Amir Hesam Salavati <saloot@gmail.com>
2016-05-09 15:13:28 +0200

    experimenting with the new parallelization scheme

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -3731,7 +3731,7 @@ def inference_constraints_hinge_parallel(out_spikes_tot_mat_file,TT,block_size,n
                 else:
                     #..........In Parallel, Process Spike Files..........
                     int_results = []
-                    for inp in range(0,num_process_calc_spike):
+                    for inp in range(0,num_process):
                         block_start = block_start_inds[itr_block]
                         t_start = block_start
                         t_end = block_start + block_size
