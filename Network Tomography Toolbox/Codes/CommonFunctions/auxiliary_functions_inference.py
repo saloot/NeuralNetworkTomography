1f57e92f2f996df67f6980e5108607e96828b94d
Amir Hesam Salavati <saloot@gmail.com>
2016-05-10 13:08:15 +0200

    the code is now reverted back to the case with fetch and optimize scheme

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -3693,11 +3693,14 @@ def inference_constraints_hinge_parallel(out_spikes_tot_mat_file,TT,block_size,n
                     
                 lambda_temp = lambda_tot[t_start:t_end_w]
                 
-                func_args = [W_tot,A[t_start-block_start_w:t_end_w-block_start_w,:],YA[t_start-block_start_w:t_end_w-block_start_w],gg,lambda_temp,rand_sample_flag,mthd,len_v,t_start,t_end_w]
+                aa = A[t_start-block_start_w:t_end_w-block_start_w,:]
+                yy = YA[t_start-block_start_w:t_end_w-block_start_w]
+                func_args = [W_tot,aa,yy,gg,lambda_temp,rand_sample_flag,mthd,len_v,t_start,t_end_w]
                 #infer_w_block(W_tot,A[t_start-block_start_w:t_end_w-block_start_w,:],YA[t_start-block_start_w:t_end_w-block_start_w],gg,lambda_temp,rand_sample_flag,mthd,len_v,t_start,t_end_w)
                 #pdb.set_trace()
                 int_results.append(pool.apply_async(infer_w_block, func_args) )
                 t_end_last_w = t_end_w
+                print 'oops %s' %str(sum(YA==0))
             #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
             
