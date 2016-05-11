2edaf74add868b75f21f89892aff40ca93d6a349
Amir Hesam Salavati <saloot@gmail.com>
2016-05-11 15:58:30 +0200

    experimenting with the perceptron method

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -4032,7 +4032,7 @@ def infer_w_block(W_in,aa,yy,gg,lambda_temp,rand_sample_flag,mthd,len_v,t_start,
         else:
             #Delta_W_loc = np.reshape(aa_t,[len_v-1,1]) * 0.5 * (np.sign(xx-1) + np.sign(xx-10)))
             d_alp = max(0,1-np.dot(W_temp.T,ff))
-            Delta_W_loc = 0.001**d_alp * np.reshape(aa_t,[len_v-1,1])
+            Delta_W_loc = 0.001*d_alp * np.reshape(aa_t,[len_v-1,1])
             #Delta_W_loc = 0.001*(np.reshape(aa_t,[len_v-1,1]) * max(0,1-np.dot(W_temp.T,ff)))
                 
         Delta_W = Delta_W + Delta_W_loc
