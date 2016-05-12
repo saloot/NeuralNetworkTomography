2a58bb1338187023afa64e7a2cdecddfa562cbcb
Amir Hesam Salavati <saloot@gmail.com>
2016-05-12 17:04:43 +0200

    experimenting with step size for mthd 4

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -4045,7 +4045,7 @@ def infer_w_block(W_in,aa,yy,gg,lambda_temp,rand_sample_flag,mthd,len_v,t_start,
             if d_alp:
                 Delta_W_loc = d_alp*np.reshape(aa_t,[len_v-1,1])/(0.0001+pow(np.linalg.norm(aa_t),2))
                 Delta_W = Delta_W + Delta_W_loc
-                #pdb.set_trace()
+                pdb.set_trace()
                 
                 
             #Delta_W_loc = 0.001*(np.reshape(aa_t,[len_v-1,1]) * max(0,1-np.dot(W_temp.T,ff)))
