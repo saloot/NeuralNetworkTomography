4e0304967fb3d7ba844d7aebc2aa725d9f38a653
Amir Hesam Salavati <saloot@gmail.com>
2016-05-14 00:49:22 +0200

    changingthe c parameter

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -4068,13 +4068,14 @@ def infer_w_block(W_in,aa,yy,gg,lambda_temp,rand_sample_flag,mthd,len_v,t_start,
                     Delta_W_loc = Delta_W_loc/float(no_zeros)
                 
                 Delta_W_loc = Delta_W_loc - 0.000001*W_temp
+                Delta_W_loc[-1] = 0.1
                 Delta_W = Delta_W + Delta_W_loc
                 W_temp = W_temp + Delta_W_loc
                 
                 
             #Delta_W_loc = 0.001*(np.reshape(aa_t,[len_v-1,1]) * max(0,1-np.dot(W_temp.T,ff)))
                 
-        W_temp[-1] = .1
+        
         #W_temp_last = W_temp
         #W_temp = W_temp + Delta_W_loc
         
