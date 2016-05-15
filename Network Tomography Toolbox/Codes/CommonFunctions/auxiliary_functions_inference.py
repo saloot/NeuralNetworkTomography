3eb94ced6a9284bb1fcfa8c565f30ad62e0a0131
Amir Hesam Salavati <saloot@gmail.com>
2016-05-15 02:00:23 +0200

    changingthe c parameter

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -3898,7 +3898,7 @@ def infer_w_block(W_in,aa,yy,gg,lambda_temp,rand_sample_flag,mthd,len_v,t_start,
     cst_y = 0
     cst_old = 0
     class_samle_flag = 1                # If 1, we try to balance the dataset
-    sample_freq = 0.5                  # With what probability sampling class 1 or 0 should be considered
+    sample_freq = 0.1                  # With what probability sampling class 1 or 0 should be considered
     if 1:        
         ind_ones = np.nonzero(yy>0)[0]
         ind_zeros = np.nonzero(yy<0)[0]
@@ -4062,7 +4062,7 @@ def infer_w_block(W_in,aa,yy,gg,lambda_temp,rand_sample_flag,mthd,len_v,t_start,
             W_temp = W_temp + Delta_W_loc
         else:
             #Delta_W_loc = np.reshape(aa_t,[len_v-1,1]) * 0.5 * (np.sign(xx-1) + np.sign(xx-10)))
-            d_alp = max(0,.01-np.dot(W_temp.T,aa_t))
+            d_alp = max(0,.1-np.dot(W_temp.T,aa_t))
             if d_alp:
                 Delta_W_loc = d_alp*np.reshape(aa_t,[len_v-1,1])/(0.0001+pow(np.linalg.norm(aa_t),2))
                 #if yy_t > 0:
