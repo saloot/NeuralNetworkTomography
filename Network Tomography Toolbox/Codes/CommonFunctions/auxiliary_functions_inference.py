ef8d37fb9eb3b4d123c840199bebd3d79a70f08d
Amir Hesam Salavati <saloot@gmail.com>
2016-05-16 00:54:37 +0200

    changingthe c parameter

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -3776,7 +3776,7 @@ def inference_constraints_hinge_parallel(out_spikes_tot_mat_file,TT,block_size,n
                 W_tot = W_tot - W_tot.mean()
                 W_tot = W_tot/np.linalg.norm(W_tot)
                 print np.linalg.norm(W_tot)
-                sparse_thr = W_tot[:-1].std()/5.
+                sparse_thr = W_tot[:-1].std()/15.
                 W_tot[:-1] = soft_threshold(W_tot[:-1],sparse_thr)
                 print np.linalg.norm(W_tot)
                 #pdb.set_trace()
@@ -4076,7 +4076,7 @@ def infer_w_block(W_in,aa,yy,gg,lambda_temp,rand_sample_flag,mthd,len_v,t_start,
                 #else:
                 #    Delta_W_loc = Delta_W_loc/float(no_zeros)
                 
-                Delta_W_loc = Delta_W_loc - 0.001*W_temp
+                Delta_W_loc = Delta_W_loc - 0.00001*W_temp
                 Delta_W_loc[-1] = .1
                 Delta_W = Delta_W + Delta_W_loc
                 #W_temp = W_temp + Delta_W_loc
