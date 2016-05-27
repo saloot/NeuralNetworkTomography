6e7493a1220bc31462ee6a55f9735a8229e0bc16
Amir Hesam Salavati <saloot@gmail.com>
2016-05-27 16:29:49 +0200

    The inference function was cleaned as well

--- Network Tomography Toolbox/Codes/Inference_Tomography.py
+++ Network Tomography Toolbox/Codes/Inference_Tomography.py
@@ -50,7 +50,7 @@ sparse_thr0 = 0.0005                                    # The initial sparsity s
 tau_d = 20.0                                    # The decay time coefficient of the neural membrane (in the LIF model)
 tau_s = 2.0                                     # The rise time coefficient of the neural membrane (in the LIF model)
 class_sample_freq = 0.25                        # If non-zero, the spiking activities (instances of firing) are picked with this probabaility to update the weights
-
+rand_sample_flag = 1                            # If 1, the spikes are sampled randomly on intervals
 
 num_process = min(no_processes,multiprocessing.cpu_count())
 block_size = min(block_size,T)
