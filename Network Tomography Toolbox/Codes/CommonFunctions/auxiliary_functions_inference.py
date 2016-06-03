26b31ca51bd825cdc0a17da4766d96ee8c5cc78c
Amir Hesam Salavati <saloot@gmail.com>
2016-06-04 00:25:26 +0200

    deubgging

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -4034,10 +4034,11 @@ def infer_w_block(W_in,aa,yy,lambda_temp,len_v,t_start,t_end,inferece_params):
                         
                         Delta_W_loc = max(0,e0-np.dot(W_temp.T,aa_t))*np.reshape(aa_t,[len_v-1,1])
                         Delta_W_loc = np.multiply(Delta_W_loc,s)
-                        Delta_W_loc = Delta_W_loc /(0.0001+pow(np.linalg.norm(np.multiply(np.reshape(aa_t,[len_v-1,1]),s)),2))
-                        print np.linalg.norm(aa_t)
-                        print np.linalg.norm(np.multiply(np.reshape(aa_t,[len_v-1,1]),s))
-                        pdb.set_trace()
+                        if sum(s):
+                            Delta_W_loc = Delta_W_loc /(0.0001+pow(np.linalg.norm(np.multiply(np.reshape(aa_t,[len_v-1,1]),s)),2))
+                        #print np.linalg.norm(aa_t)
+                        #print np.linalg.norm(np.multiply(np.reshape(aa_t,[len_v-1,1]),s))
+                        #pdb.set_trace()
                     except RuntimeWarning:
                         pdb.set_trace()
                     
