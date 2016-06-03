1ed0d580370f7ece540ee341414a881f47500fa7
Amir Hesam Salavati <saloot@gmail.com>
2016-06-03 11:54:02 +0200

    deubgging

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -3595,7 +3595,8 @@ def inference_constraints_hinge_parallel(out_spikes_tot_mat_file,TT,block_size,n
     for t_start in range(0,block_size,t_step):
         t_end = min(block_size-1,t_start + t_step)
         
-        pdb.set_trace()
+        
+        #calculate_integration_matrix(n_ind,out_spikes_tot_mat_file,n,theta,t_start,t_end,tau_d,tau_s,kernel_choice)
         func_args = [n_ind,out_spikes_tot_mat_file,n,theta,t_start,t_end,tau_d,tau_s,kernel_choice]
         int_results.append(pool.apply_async( calculate_integration_matrix, func_args) )
         #pool.close()
