176aec57d7c0655d9be67dd16bdb5227ee970765
Amir Hesam Salavati <saloot@gmail.com>
2016-09-06 09:52:03 +0430

    new sessions added to the HC3 dataset

--- Code to Parse Other Datasets/HC-3/ParseHC3Data.py
+++ Code to Parse Other Datasets/HC-3/ParseHC3Data.py
@@ -25,10 +25,41 @@ spikes_tot = []
 n_clusters = []
 #----------------------------------------------------------------------------
 
+#--------------------------Verify the Imported Files-------------------------
+
+#.....................Map Ours and Theirs Neuron IDs.........................
+cell_id_inf = np.genfromtxt('./Data/hc3-cell.csv', dtype=str, delimiter=',')
+cell_id_inf = cell_id_inf[cell_id_inf[:,1]==task_name]
+
+neuron_id_map = {}
+
+for item in cell_id_inf:
+    cell_id = item[0]
+    electrode = int(item[3])
+    cluster = int(item[4]) - 2
+    
+    neuron_id = sum(n_clusters[0:electrode-1]) + cluster
+    
+    neuron_id_map[cell_id] = neuron_id
+#............................................................................
+
+#............................Retrieve Session ID.............................
+sessions_inf = np.genfromtxt('./Data/hc3-session.csv', dtype=str, delimiter=',')
+sessions_inf = sessions_inf[sessions_inf[:,2]==session_name]
+session_id = int(sessions_inf[0,0])
+#............................................................................
+
+#.......................Read the Actual Spike Counts.........................
+sp_counts_tot = np.genfromtxt('./Data/hc3-spike_count.csv', dtype=int, delimiter=',')
+#............................................................................
+
+#----------------------------------------------------------------------------
+
 
 #-------------------Calculate the Size of the Spikes Matrix------------------
 T_max_tot = 0
 n_tot_tot = 0
+T_max_array = []
 for session_name in session_name_list:
     clusters_file_base = './Data/' + session_name + '.clu.'
     spikes_file_base = './Data/' + session_name + '.res.'
@@ -66,7 +97,8 @@ for session_name in session_name_list:
             pdb.set_trace()
     else:
         n_tot_tot = n_tot
-        
+    
+    T_max_array.append(T_max)    
     T_max_tot = T_max_tot + T_max + t_inter_session
     
 T_max_tot = T_max_tot - t_inter_session
@@ -75,13 +107,14 @@ T_max_tot = T_max_tot - t_inter_session
 pdb.set_trace()
 
 #---------------------------Read the Spikes Matrix--------------------------
-sp_times_tot_mat = np.zeros([n_tot_tot,T_max_tot])
-
 itr_session = 0
 for session_name in session_name_list:
     clusters_file_base = './Data/' + session_name + '.clu.'
     spikes_file_base = './Data/' + session_name + '.res.'
     n_curr = 0
+    
+    sp_times_tot_mat = np.zeros([n_tot_tot,T_max_array[itr_session]])
+    
     for shank_no in range(1,no_shanks+1):
         clusters_file = clusters_file_base + str(shank_no)
         spikes_file = spikes_file_base + str(shank_no)
@@ -96,88 +129,53 @@ for session_name in session_name_list:
             ii = clusters_inds[i+1]
             if ii > 1:
                 tt = int(1000*sp_times[i]/sampling_freq)
-                sp_times_tot_mat[ii-2+n_curr,tt+itr_session*t_inter_session] = 1
+                sp_times_tot_mat[ii-2+n_curr,tt] = 1
                 #sp_times_mat[ii-2,tt] = 1
                 #sps[tt,1] = tt/1000.0
                 #sps[tt,0] = sum(n_clusters) + ii-2
     
         n_curr = n_curr + n
+
+
+
+    #....................Verify the processed spikes.........................
+    sp_counts = sp_counts_tot[sp_counts_tot[:,1]==session_id]
+    
+    aa = np.sum(sp_times_tot_mat,axis = 1)
+    troublesome_entries = []
+    troublesome_values = []
+    for item in sp_counts:
+        
+        ind = neuron_id_map[str(item[0])]
+        ff = int(item[2])
+        if (abs(aa[ind] - ff)>1):
+            troublesome_entries.append(ind)
+            troublesome_values.append(abs(aa[ind] - ff))
+            
+    if sum(troublesome_values)/float(sum(aa[troublesome_entries]))< 0.001:
+        verify_flag = 1
+    else:
+        verify_flag = 0    
+    #........................................................................
+        
+    #..........................Save the Results..............................
+    if verify_flag:
+        pdb.set_trace()
+        inds = np.nonzero(sp_times_tot_mat)
+        spikes_tot = np.zeros([len(inds[0]),2])
+        spikes_tot[:,0] = inds[0]
+        spikes_tot[:,1] = (inds[1]+itr_session*t_inter_session)/1000.0
+        #for i in range(0,len(inds[0])):
+        #    spikes_tot[i,0] = inds[0][i]
+        #    spikes_tot[i,1] = inds[1][i]/1000.0
+        
+        neural_activity_file_write_temp = neural_activity_file_write + session_name
+        np.savetxt(neural_activity_file_write_temp,spikes_tot,'%3.5f',delimiter='\t')
+    #........................................................................
     
     itr_session = itr_session + 1
-    #if shank_no>1:
-    #    sp_times_tot_mat = np.vstack([sp_times_tot_mat,sp_times_mat])
-    #    #spikes_tot = np.vstack([spikes_tot,sps])
-    #else:
-    #    sp_times_tot_mat = sp_times_mat
-    #    #spikes_tot = sps
 #----------------------------------------------------------------------------
 
-#--------------------------Verify the Imported Files-------------------------
-
-#.....................Map Ours and Theirs Neuron IDs.........................
-cell_id_inf = np.genfromtxt('./Data/hc3-cell.csv', dtype=str, delimiter=',')
-cell_id_inf = cell_id_inf[cell_id_inf[:,1]==task_name]
-
-neuron_id_map = {}
-
-for item in cell_id_inf:
-    cell_id = item[0]
-    electrode = int(item[3])
-    cluster = int(item[4]) - 2
-    
-    neuron_id = sum(n_clusters[0:electrode-1]) + cluster
-    
-    neuron_id_map[cell_id] = neuron_id
-#............................................................................
-
-#............................Retrieve Session ID.............................
-sessions_inf = np.genfromtxt('./Data/hc3-session.csv', dtype=str, delimiter=',')
-sessions_inf = sessions_inf[sessions_inf[:,2]==session_name]
-session_id = int(sessions_inf[0,0])
-#............................................................................
-
-#.......................Read the Actual Spike Counts.........................
-sp_counts = np.genfromtxt('./Data/hc3-spike_count.csv', dtype=int, delimiter=',')
-sp_counts = sp_counts[sp_counts[:,1]==session_id]
-#............................................................................
-
-
-#...........................Verify the Results...............................
-aa = np.sum(sp_times_tot_mat,axis = 1)
-troublesome_entries = []
-troublesome_values = []
-for item in sp_counts:
-    
-    ind = neuron_id_map[str(item[0])]
-    ff = int(item[2])
-    if (abs(aa[ind] - ff)>1):
-        troublesome_entries.append(ind)
-        troublesome_values.append(abs(aa[ind] - ff))
-        
-if sum(troublesome_values)/float(sum(aa[troublesome_entries]))< 0.001:
-    verify_flag = 1
-else:
-    verify_flag = 0
-#............................................................................
-
-#----------------------------------------------------------------------------
-
-
-#------------------------Write the Spikes to the File------------------------
-if verify_flag:
-    pdb.set_trace()
-    inds = np.nonzero(sp_times_tot_mat)
-    spikes_tot = np.zeros([len(inds[0]),2])
-    spikes_tot[:,0] = inds[0]
-    spikes_tot[:,1] = inds[1]/1000.0
-    #for i in range(0,len(inds[0])):
-    #    spikes_tot[i,0] = inds[0][i]
-    #    spikes_tot[i,1] = inds[1][i]/1000.0
-        
-        
-    
-    np.savetxt(neural_activity_file_write,spikes_tot,'%3.5f',delimiter='\t')    
-#----------------------------------------------------------------------------
 
 #-----------------Parse the Properties of Ground Truth Graph-----------------
 # Each row in the following matrix described properties of one neuron
