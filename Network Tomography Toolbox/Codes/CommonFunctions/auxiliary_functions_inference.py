4069407e72c3980e114b9be51e4da8832c88bf19
Amir Hesam Salavati <saloot@gmail.com>
2016-09-26 14:07:33 +0330

    starting to figure out what happens to HC3

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -3659,8 +3659,8 @@ def inference_constraints_hinge_parallel(out_spikes_tot_mat_file,TT,block_size,n
                 else:
                     lambda_temp = []
         
-                #infer_w_block(W_tot,A,YA,lambda_tot[block_start_w:block_end_w],len_v,block_start_w,block_end_w,inferece_params)
-                #pdb.set_trace()
+                infer_w_block(W_tot,A,YA,lambda_tot[block_start_w:block_end_w],len_v,block_start_w,block_end_w,inferece_params)
+                pdb.set_trace()
                     
                 if 0:#not (itr_cost%2):
                     func_args = [np.zeros([len_v-1,1]),A[t_start-block_start_w:t_end_w-block_start_w,:],YA[t_start-block_start_w:t_end_w-block_start_w],lambda_temp,len_v,t_start,t_end_w,inferece_params]
