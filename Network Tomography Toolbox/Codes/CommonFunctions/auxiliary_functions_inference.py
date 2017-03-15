b02156cdfccc560d3415f408932b193dcf8429b2
Amir Hesam Salavati <saloot@gmail.com>
2017-03-16 01:07:59 +0330

    trying to debug the cuser issue

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -3692,6 +3692,7 @@ def inference_constraints_hinge_parallel(out_spikes_tot_mat_file,TT,block_size,n
                 continue
                         
             func_args = [n_ind,out_spikes_tot_mat_file,n,theta,t_start,t_end,tau_d,tau_s,kernel_choice,hidden_neurons]
+            aa,yy,tt_start,tt_end,spike_flag,memory_used = calculate_integration_matrix(n_ind,out_spikes_tot_mat_file,n,theta,t_start,t_end,tau_d,tau_s,kernel_choice,hidden_neurons)
             int_results.append(pool.apply_async( calculate_integration_matrix, func_args) )
             t_end_last_t = t_end
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -3703,6 +3704,7 @@ def inference_constraints_hinge_parallel(out_spikes_tot_mat_file,TT,block_size,n
         mem_temp = 0
         for result in int_results:
             (aa,yy,tt_start,tt_end,spike_flag,memory_used) = result.get()
+            
             mem_temp = mem_temp + memory_used
             if spike_flag < 0:
                 A[tt_start-block_start:tt_end-block_start,:] = aa
