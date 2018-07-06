11f05ec5ea1a2702f0c6fc09e8baa74e9a7a48b2
Amir Hesam Salavati <saloot@gmail.com>
2018-07-06 18:51:49 +0430

    bckup

--- Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
+++ Network Tomography Toolbox/Codes/CommonFunctions/auxiliary_functions_inference.py
@@ -509,7 +509,7 @@ def inference_constraints_hinge_parallel(out_spikes_tot_mat_file,TT,block_size,n
                     lambda_temp = []
         
                 inferece_params[10] = 0
-                Delta_W,d_alp_vec,t_start,t_end,cst,memory_used  = infer_w_block(W_tot,A,YA,lambda_temp,len_v,0,block_size,inferece_params)
+                #Delta_W,d_alp_vec,t_start,t_end,cst,memory_used  = infer_w_block(W_tot,A,YA,lambda_temp,len_v,0,t_step_w,inferece_params)
                 
                 #pdb.set_trace()
                     
